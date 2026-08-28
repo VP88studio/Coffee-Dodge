e544be92b1897f7833a425dfccf7c3165b353739
Jimmithy <jim19946ist@gmail.com>
2026-08-28 23:16:28 +1200

    PIPES

--- main.py
+++ main.py
@@ -24,37 +24,43 @@ birdimg = pygame.transform.scale(birdold, (120, 80))
 #birdheight 1024
 #3:2 ratio
 #Game Vars and Functions
+first_pillary = random.randint(-280, -70)
+second_pillary = random.randint(-280, -70)
+third_pillary = random.randint(-280, -70)
 keys = pygame.key.get_pressed()
 birdy = 460
 pillarx = []
 pillar1x = 600
 pillar2x = 900
+pillar3x = 1200
+pillarx.append(pillar1x)
+pillarx.append(pillar2x)
+pillarx.append(pillar3x)
 def pillarxlistmaker():
-    pillarx.append(pillar1x)
-    pillarx.append(pillar2x)
+    pillarx[0] = pillar1x
+    pillarx[1] = pillar2x
+    pillarx[2] = pillar3x
 pillarxlistmaker()
-def pillaryfinder():
-    global first_pillary, second_pillary, third_pillary
-    #every 209.8
-    first_pillary = random.randint(-280, -70)
-    second_pillary = random.randint(-280, -70)
-    third_pillary = random.randint(-280, 70)
-pillaryfinder()
 class pillarobj:
     def __init__(self):
         self.pillarylist = []
         self.xposlist = []
         self.xposlist.append(pillarx[0])
         self.xposlist.append(pillarx[1])
+        self.xposlist.append(pillarx[2])
         self.pillarylist.append(first_pillary)
         self.pillarylist.append(second_pillary)
+        self.pillarylist.append(third_pillary)
         self.img  = pillar
         self.imgflip = pillarflip
         self.pillar1 = display.blit(self.img, (self.xposlist[0], self.pillarylist[0]))
         self.pillar1flip = display.blit(self.imgflip, (self.xposlist[0], self.pillarylist[0] + 850))
         self.pillar2 = display.blit(self.img, (self.xposlist[1], self.pillarylist[1]))
         self.pillar2flip = display.blit(self.imgflip, (self.xposlist[1], self.pillarylist[1] + 850))
-#gets width of the backround image 
+        self.pillar3 = display.blit(self.img, (self.xposlist[2], self.pillarylist[2]))
+        self.pillar3flip = display.blit(self.imgflip, (self.xposlist[2], self.pillarylist[2] + 850))
+        
+#gets width     of the backround image 
 backround_width = backround.get_width()
 #sets the backround x position
 backroundx = 0
@@ -90,9 +96,16 @@ while running:
     bird_hightspeed += (0.125)
     birdy += bird_hightspeed
     #pillar move loop
+    pillarxlistmaker()
     pillar1x -= scrollspeed
     pillar2x -= scrollspeed
-    print(pillar1x, pillar2x)
+    if pillar1x == -260:
+        pillar1x = 600
+        first_pillary = random.randint(-280, -70)
+    if pillar2x == -260:
+        pillar2x = 600
+        second_pillary = random.randint(-280, -70)
+    third_pillary = random.randint(-280, 70)
     
     #makes backroundx = to backroundx - scrollspeed so every time it loops it it is equal to itself - scroll speed to make it move
     backroundx -= scrollspeed   
@@ -105,8 +118,10 @@ while running:
     #idk how i didnt think of this in the 3 days i spent on this problem
     display.blit(backround, (backroundx + backround_width, 0))
     display.blit(birdimg, (290, birdy))
-    pillarimgthingy = pillarobj()
-    pillarimgthingy.pillar1
-    pillarimgthingy.pillar2
+    pillarload = pillarobj()
+    pillarload.pillar1
+    pillarload.pillar2
+    pillarload.pillar3
+    print(pillarload.xposlist, pillarx[0], pillarx[1])
     pygame.display.update()
     clock.tick(60)
