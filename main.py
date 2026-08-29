d9c4229f10f990688dcb926f463341f2595e6956
Jimmithy <jim19946ist@gmail.com>
2026-08-30 00:15:04 +1200

    Start Menu Added

--- main.py
+++ main.py
@@ -14,20 +14,57 @@ color = 255, 0, 0
 clock = pygame.time.Clock()
 #Font
 font = pygame.font.Font('Assets/font.ttf', 120)
+mode_font = pygame.font.Font('Assets/font.ttf', 70)
 #Backround
 backroundold = pygame.image.load('Assets/backround.png')
 backround = pygame.transform.scale(backroundold, (1049, 1499))
 backroundflip = pygame.transform.rotate(backround, 180)
+#Home Screen
+homeold = pygame.image.load('Assets/home.png')
+homeimg = pygame.transform.scale(homeold, (750, 1000))
+BLUE = 0,0,255
+startbutton = {
+    'x': 180,
+    'y': 350,
+    'width': 340,
+    'height': 100
+}
+easybutton = {
+    'x': 200,
+    'y': 475,
+    'width': 300,
+    'height': 60
+}
+normalbutton = {
+    'x': 200,
+    'y': 545,
+    'width': 300,
+    'height': 60
+}
+hardbutton = {
+    'x': 200,
+    'y': 620,
+    'width': 300,
+    'height': 60
+}
 #GameOverScreen
 gameoverold = pygame.image.load('Assets/gameover.png')
 gameoverimg = pygame.transform.scale(gameoverold, (750, 1070))
 restartbuttonold = pygame.image.load('Assets/restartbutton.png')
 restartbuttonimg = pygame.transform.scale(restartbuttonold, (400, 300))
+homebuttonold = pygame.image.load('Assets/homebutton.png')
+homebuttonimg = pygame.transform.scale(homebuttonold, (400, 300))
 restartbutton = {
     'x': 150,
     'y': 500,
     'width': 400,
     'hight': 300
+}
+homebutton = {
+    'x': 150,
+    'y': 700,
+    'width': 400,
+    'height': 300
 }
 #Pillar
 pillarold = pygame.image.load('Assets/pillar.png') 
@@ -44,11 +81,16 @@ highscore = {
     "highscore": '0'
 }
 with open('Saves/saves.txt', "r") as file:
-    highscore['highscore'] = int(file.read())
+    if file.read() == '':
+        with open('Saves/saves.txt', "w") as file:
+            file.write('0')
+            print('No Save Data')
+    else:
+        highscore['highscore'] = str(file.read())
 WHITE = 255,255,255
 score = 0
 resetgame = False
-gamestatus = 3
+gamestatus = 1
 birdx = 290
 bird_mask = pygame.mask.from_surface(birdimg)
 pillar_mask = pygame.mask.from_surface(pillar)
@@ -102,13 +144,25 @@ bird_hightspeed = 0
 #Run Loop
 running = True
 while running:
-    if gamestatus == 2:
-        if score >= int(highscore['highscore']):
-            highscore['highscore'] = str(score)
-            print(highscore['highscore'])
-        mousepos = pygame.mouse.get_pos()
-        restart_mask = pygame.mask.from_surface(restartbuttonimg)
-        restart_rect = pygame.Rect((restartbutton['x'], restartbutton['y']), (restartbutton['width'], restartbutton['hight']))
+    if scrollspeed == 1:
+        mode = 'Mode: Easy'
+        modex = 155
+    if scrollspeed == 2:
+        mode = 'Mode: Normal'
+        modex = 100
+    if scrollspeed == 4:
+        mode = "Mode: Hard"
+        modex = 145
+    mousepos = pygame.mouse.get_pos()
+    if gamestatus == 1:
+        #home backround DONT MOVE UNLESS U WANNA SPEND 3 HOURS DYING ON THE INSIDE
+        display.blit(homeimg, (-25,0))
+        mode_surface = mode_font.render(mode, True, (230, 115, 0))
+        display.blit(mode_surface, (modex, 10))
+        start_rect = pygame.Rect((startbutton['x'], startbutton['y']), (startbutton['width'], startbutton['height']))
+        easy_rect = pygame.Rect((easybutton['x'], easybutton['y']), (easybutton['width'], easybutton['height']))
+        normal_rect = pygame.Rect((normalbutton['x'], normalbutton['y']), (normalbutton['width'], normalbutton['height']))
+        hard_rect = pygame.Rect((hardbutton['x'], hardbutton['y']), (hardbutton['width'], hardbutton['height']))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 with open('Saves/saves.txt', "w") as file:
@@ -116,11 +170,39 @@ while running:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rect.collidepoint(mousepos):
+                    gamestatus = 3
+                if easy_rect.collidepoint(mousepos):
+                    scrollspeed = 1
+                if normal_rect.collidepoint(mousepos):
+                    scrollspeed = 2
+                if hard_rect.collidepoint(mousepos):
+                    scrollspeed = 4
+    if gamestatus == 2:
+        if highscore['highscore'] == '':
+            highscore['highscore'] = '0'
+        if score >= int(highscore['highscore']):
+            highscore['highscore'] = str(score)
+            print(highscore['highscore'])
+        restart_rect = pygame.Rect((restartbutton['x'], restartbutton['y']), (restartbutton['width'], restartbutton['hight']))
+        home_rect = pygame.Rect((homebutton['x'], homebutton['y']), (homebutton['width'], homebutton['height']))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                with open('Saves/saves.txt', "w") as file:
+                    file.write(str(highscore['highscore'])) 
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if restart_rect.collidepoint(mousepos):
                     resetgame = True
                     gamestatus = 3
+                if home_rect.collidepoint(mousepos):
+                    resetgame = True
+                    gamestatus = 1
+        
         display.blit(gameoverimg, (-25, -35))
-        display.blit(restartbuttonimg, (150, 500))
+        display.blit(homebuttonimg, (homebutton['x'], homebutton['y']))
+        display.blit(restartbuttonimg, (restartbutton['x'], restartbutton['y']))
         
     if gamestatus == 3:
         if resetgame:
@@ -192,15 +274,15 @@ while running:
         pillarflip2_offset = (pillar2_offsetx, pillarflip2_offsety)
         pillarflip3_offset = (pillar3_offsetx, pillarflip3_offsety)
         #score
-        if birdx == pillar1x + 200:
+        if birdx == pillar1x + 202:
             score += 1
             if testscore:
                 print(score)
-        if birdx == pillar2x + 200:
+        if birdx == pillar2x + 202:
             score += 1
             if testscore:
                 print(score)
-        if birdx == pillar3x + 200:
+        if birdx == pillar3x + 202:
             score += 1
             if testscore:
                 print(score)
@@ -231,7 +313,7 @@ while running:
         if testpillar == True:
             print(pillarload.xposlist, pillarx[0], pillarx[1], pillarx[2])
         #fontimage
-        score_surface = font.render(str(score), True, WHITE)
+        score_surface = font.render(str(score), True, (230, 115, 0))
         display.blit(score_surface, (300, 0))
     #reset screen
     pygame.display.update()
